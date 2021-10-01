4f55dd4c9efea09bab5f310999b9f3fc33271f6f
WQdream <81961320+WQdream@users.noreply.github.com>
2021-10-01 21:25:07 +0800

    Add files via upload

---
+++ docs/index.ts
@@ -0,0 +1,9 @@
+import './style/index.less'
+// import Food from './moduls/Food' 
+// import ScorePanel from './moduls/ScorePanel' 
+// import Snack from './moduls/snack'
+
+import GameControl from './moduls/GameControl'
+
+new GameControl();
+
